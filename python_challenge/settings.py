98547be06528204df02adf35796c59cd273666bd
Ozzie Cunningham <oswald3.cunningham@gmail.com>
2020-03-21 17:22:44 -0400

    base setting needed for app

--- python_challenge/settings.py
+++ python_challenge/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'python_challenge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'python_challenge',
-        'USER': 'admin28',
-        'PASSWORD': 'python',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
